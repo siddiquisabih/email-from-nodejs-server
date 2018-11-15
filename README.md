836ccd34361755d995e21417053b1f741e52e19a
sabih <siddiquisabih94@gmail.com>
2018-11-15 18:17:18 +0500

    send email from node server to any email account

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # email-from-nodejs-server
+# run npm install 
---
+++ program.js
@@ -0,0 +1,54 @@
+const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+
+
+
+const oauth2Client = new OAuth2(
+    "Client id here",
+    "Client Secret id here", // Client Secret
+    "Redirect URL" //  Redirect URL OauthPlayGround link
+);
+
+
+oauth2Client.setCredentials({
+    refresh_token: "refresh_token" // refresh token
+});
+
+const accessToken = oauth2Client.refreshAccessToken()
+    .then(res => res.credentials.access_token);
+
+
+
+
+
+const smtpTransport = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        type: "OAuth2",
+        user: "userEmail here", // your account
+        clientId: "Client id here",
+        clientSecret: "Client Secret id here",
+        refreshToken: "refresh_token here",
+        accessToken: accessToken
+    }
+});
+
+//<img src="logo.png" style="height: 30%;width: 30%" alt="Coin Grace" href="./logo.png">
+ 
+ 
+
+
+const mailOptions = {
+    from: "me@gmail.com", //sender email
+    to: "someone@expample.com", //receiver email
+    subject: "Node js email server", //subject
+    generateTextFromHTML: true, 
+    html: `<h1>first email</h1>` //html content  if any 
+};
+
+
+smtpTransport.sendMail(mailOptions, (error, response) => {
+    error ? console.log(error) : console.log(response);
+    smtpTransport.close();
+});
